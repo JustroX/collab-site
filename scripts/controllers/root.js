0f01b16f7b5bac406ab380b6d4b96e60555f293a
Justine <thepiesaresquared@gmail.com>
2019-03-13 20:41:55 +0800

    autofollow admin

--- scripts/controllers/root.js
+++ scripts/controllers/root.js
@@ -12,6 +12,14 @@ app.controller("rootController",function($scope,$http,$location,$timeout,$rootSc
 					$scope.$broadcast('ready');
 				},10);
 			});
+
+			//check if following admin
+			for(let u of $scope.SESSION_USER.followed_by)
+			{
+				if(u.user == "5c57d3d7cc14d03be00ba0b4") return;
+			}
+
+			$http.post("/api/user/5c57d3d7cc14d03be00ba0b4/followed_by",{});
 		});
 	});
 
